33de9a8cfc99892fb4b9716e6bd756495e9971fa
Konstantinos Lypitkas <klipitkas@gmail.com>
2021-06-28 18:30:38 +0300

    Retrieve collections count from resource

--- app/Http/Resources/v1/TeamResourceResource.php
+++ app/Http/Resources/v1/TeamResourceResource.php
@@ -33,6 +33,7 @@ class TeamResourceResource extends JsonResource
             'issued_at' => $this->issued_at,
             'author_id' => $this->author_id,
             'publisher_id' => $this->publisher_id,
+            'collections_count' => $this->collections->count(),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
